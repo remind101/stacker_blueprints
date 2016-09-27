5839d76a0e29a3fa6b07a460ff3f0d8cf9b889b7
Michael Barrett <loki77@gmail.com>
2016-09-27 12:31:41 -0700

    Remove alpha release

--- setup.py
+++ setup.py
@@ -24,7 +24,7 @@ def read(filename):
 if __name__ == "__main__":
     setup(
         name="stacker_blueprints",
-        version="0.7.1a1",
+        version="0.7.0",
         author="Michael Barrett",
         author_email="loki77@gmail.com",
         license="New BSD license",
