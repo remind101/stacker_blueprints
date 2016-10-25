0afc72f939db342c36edb0fca1e347e581b0aaf8
Michael Barrett <loki77@gmail.com>
2016-10-24 17:48:39 -0700

    Fully convert firehose bp to variables
    This does away with the CFN types, and is backwards incompatible as it
    allowed me to move away from CommaDelimetedLists to plain lists, which
    will make the config easier to read/write.

--- stacker_blueprints/firehose.py
+++ stacker_blueprints/firehose.py
@@ -12,27 +12,19 @@ from awacs.aws import (
 import awacs.logs
 import awacs.s3
 import awacs.kms
+import awacs.firehose
 from awacs import sts
 from stacker.blueprints.base import Blueprint
-from stacker.blueprints.variables.types import (
-    CFNCommaDelimitedList,
-    CFNString,
-)
 from troposphere import (
     iam,
     kms,
     s3,
-    Equals,
     GetAtt,
-    If,
     Join,
-    Not,
-    Or,
     Output,
     Ref,
 )
 
-from troposphere import Condition as TropoCondition
 
 BUCKET = 'S3Bucket'
 IAM_ROLE = 'IAMRole'
@@ -73,11 +65,11 @@ def firehose_write_policy():
         Statement(
             Effect=Allow,
             Action=[
-                FirehoseAction("CreateDeliveryStream"),
-                FirehoseAction("DeleteDeliveryStream"),
-                FirehoseAction("DescribeDeliveryStream"),
-                FirehoseAction("PutRecord"),
-                FirehoseAction("PutRecordBatch"),
+                awacs.firehose.CreateDeliveryStream,
+                awacs.firehose.DeleteDeliveryStream,
+                awacs.firehose.DescribeDeliveryStream,
+                awacs.firehose.PutRecord,
+                awacs.firehose.PutRecordBatch,
             ],
             Resource=['*'],
         ),
@@ -198,65 +190,63 @@ def kms_key_policy(key_use_arns, key_admin_arns):
 
 
 class Firehose(Blueprint):
-
     VARIABLES = {
         "RoleNames": {
-            "type": CFNCommaDelimitedList,
+            "type": list,
             "description": "A list of role names that should have access to "
                            "write to the firehose stream.",
-            "default": "",
+            "default": [],
         },
         "GroupNames": {
-            "type": CFNCommaDelimitedList,
+            "type": list,
             "description": "A list of group names that should have access to "
                            "write to the firehose stream.",
-            "default": "",
+            "default": [],
         },
         "UserNames": {
-            "type": CFNCommaDelimitedList,
+            "type": list,
             "description": "A list of user names that should have access to "
                            "write to the firehose stream.",
-            "default": "",
+            "default": [],
         },
         "BucketName": {
-            "type": CFNString,
+            "type": str,
             "description": "Name for the S3 Bucket",
         },
         "EncryptS3Bucket": {
-            "type": CFNString,
+            "type": bool,
             "description": "If set to true, a KMS key will be created to use "
                            "for encrypting the S3 Bucket's contents. If set "
                            "to false, no encryption will occur. Default: true",
-            "allowed_values": ["true", "false"],
-            "default": "true",
+            "default": True,
         },
         "EnableKeyRotation": {
-            "type": CFNString,
+            "type": bool,
             "description": "Whether to enable key rotation on the KMS key "
                            "generated if EncryptS3Bucket is set to true. "
                            "Default: true",
-            "allowed_values": ["true", "false"],
-            "default": "true",
+            "default": True,
         },
         "KeyUseArns": {
             "type": list,
-            "default": [],
             "description": "A list of ARNs that need access to the KMS key "
-                           "created with EncryptS3Bucket"},
+                           "created with EncryptS3Bucket",
+            "default": [],
+        },
         "KeyAdminArns": {
             "type": list,
+            "description": "A list of ARNs that need to admin the KMS key "
+                           "created with EncryptS3Bucket",
             "default": [],
-            "description": "A list of ARNs that need access to admin the KMS "
-                           "key created with EncryptS3Bucket"},
+        }
     }
 
     def create_kms_key(self):
         t = self.template
         variables = self.get_variables()
-        t.add_condition(
-            "EncryptS3Bucket",
-            Not(Equals(Ref("EncryptS3Bucket"), "false"))
-        )
+
+        if not variables["EncryptS3Bucket"]:
+            return
 
         key_description = Join(
             "",
@@ -279,7 +269,6 @@ class Firehose(Blueprint):
                 Enabled=True,
                 EnableKeyRotation=Ref("EnableKeyRotation"),
                 KeyPolicy=kms_key_policy(key_use_arns, key_admin_arns),
-                Condition="EncryptS3Bucket"
             )
         )
         key_arn = Join(
@@ -297,10 +286,14 @@ class Firehose(Blueprint):
 
     def create_bucket(self):
         t = self.template
+        variables = self.get_variables()
+
+        bucket_name = variables.get("BucketName", Ref("AWS::NoValue"))
+
         t.add_resource(
             s3.Bucket(
                 BUCKET,
-                BucketName=Ref('BucketName'),
+                BucketName=bucket_name,
             )
         )
         t.add_output(Output('Bucket', Value=Ref(BUCKET)))
@@ -311,7 +304,7 @@ class Firehose(Blueprint):
         s3_policy = iam.Policy(
             S3_WRITE_POLICY,
             PolicyName='{}-s3-write'.format(name_prefix),
-            PolicyDocument=s3_write_policy(Ref('BucketName')),
+            PolicyDocument=s3_write_policy(Ref(BUCKET)),
         )
         logs_policy = iam.Policy(
             LOGS_WRITE_POLICY,
@@ -349,63 +342,39 @@ class Firehose(Blueprint):
         ns = self.context.namespace
         name_prefix = "%s-%s" % (ns, self.name)
         t = self.template
+        variables = self.get_variables()
 
-        t.add_condition(
-            'ExternalRoles',
-            Not(Equals(Join(",", Ref('RoleNames')), '')),
-        )
-        t.add_condition(
-            'ExternalGroups',
-            Not(Equals(Join(",", Ref('GroupNames')), '')),
-        )
-        t.add_condition(
-            'ExternalUsers',
-            Not(Equals(Join(",", Ref('UserNames')), '')),
-        )
+        external_roles = variables.get("ExternalRoles", Ref("AWS::NoValue"))
+        external_groups = variables.get("ExternalGroups", Ref("AWS::NoValue"))
+        external_users = variables.get("ExternalUsers", Ref("AWS::NoValue"))
 
-        t.add_condition(
-            'CreatePolicy',
-            Or(
-                TropoCondition("ExternalRoles"),
-                TropoCondition("ExternalGroups"),
-                TropoCondition("ExternalUsers"),
+        create_policy = any([
+            variables["ExternalRoles"],
+            variables["ExternalGroups"],
+            variables["ExternalUsers"],
+        ])
+
+        if create_policy:
+            t.add_resource(
+                iam.PolicyType(
+                    FIREHOSE_WRITE_POLICY,
+                    PolicyName='{}-firehose'.format(name_prefix),
+                    PolicyDocument=firehose_write_policy(),
+                    Roles=external_roles,
+                    Groups=external_groups,
+                    Users=external_users,
+                ),
             )
-        )
-
-        t.add_resource(
-            iam.PolicyType(
-                FIREHOSE_WRITE_POLICY,
-                PolicyName='{}-firehose'.format(name_prefix),
-                PolicyDocument=firehose_write_policy(),
-                Roles=If("ExternalRoles",
-                         Ref("RoleNames"),
-                         Ref("AWS::NoValue")),
-                Groups=If("ExternalGroups",
-                          Ref("GroupNames"),
-                          Ref("AWS::NoValue")),
-                Users=If("ExternalUsers",
-                         Ref("UserNames"),
-                         Ref("AWS::NoValue")),
-                Condition='CreatePolicy',
-            ),
-        )
-        t.add_resource(
-            iam.PolicyType(
-                LOGS_POLICY,
-                PolicyName='{}-logs'.format(name_prefix),
-                PolicyDocument=logs_policy(),
-                Roles=If("ExternalRoles",
-                         Ref("RoleNames"),
-                         Ref("AWS::NoValue")),
-                Groups=If("ExternalGroups",
-                          Ref("GroupNames"),
-                          Ref("AWS::NoValue")),
-                Users=If("ExternalUsers",
-                         Ref("UserNames"),
-                         Ref("AWS::NoValue")),
-                Condition='CreatePolicy',
-            ),
-        )
+            t.add_resource(
+                iam.PolicyType(
+                    LOGS_POLICY,
+                    PolicyName='{}-logs'.format(name_prefix),
+                    PolicyDocument=logs_policy(),
+                    Roles=external_roles,
+                    Groups=external_groups,
+                    Users=external_users,
+                ),
+            )
 
     def create_template(self):
         self.create_kms_key()
