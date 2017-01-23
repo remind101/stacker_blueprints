e0c38aca4c089339c706ab4c070377dd964dedb2
Michael Barrett <loki77@gmail.com>
2017-01-22 23:01:09 -0800

    Updates RDS blueprints to fully use variables
    This also updates the configs to match.

--- stacker_blueprints/rds/mysql.py
+++ stacker_blueprints/rds/mysql.py
@@ -5,23 +5,6 @@ class MySQLMixin(object):
     def engine(self):
         return "MySQL"
 
-    def get_engine_versions(self):
-        return [
-            '5.1.73a', '5.1.73b',
-            '5.5.40', '5.5.40a', '5.5.40b', '5.5.41', '5.5.42', '5.5.46',
-            '5.6.19a', '5.6.19b', '5.6.21', '5.6.21b', '5.6.22', '5.6.23',
-            '5.6.27', '5.6.29',
-            '5.7.10', '5.7.11'
-        ]
-
-    def get_db_families(self):
-        return [
-            "mysql5.1",
-            "mysql5.5",
-            "mysql5.6",
-            "mysql5.7",
-        ]
-
 
 class MasterInstance(MySQLMixin, MasterInstance):
     pass
--- stacker_blueprints/rds/postgres.py
+++ stacker_blueprints/rds/postgres.py
@@ -5,21 +5,6 @@ class PostgresMixin(object):
     def engine(self):
         return "postgres"
 
-    def get_engine_versions(self):
-        return [
-            '9.3.1', '9.3.2', '9.3.3', '9.3.5', '9.3.6', '9.3.9', '9.3.10',
-            '9.3.12',
-            '9.4.1', '9.4.4', '9.4.5', '9.4.7',
-            '9.5.2',
-        ]
-
-    def get_db_families(self):
-        return [
-            "postgres9.3",
-            "postgres9.4",
-            "postgres9.5",
-        ]
-
 
 class MasterInstance(PostgresMixin, MasterInstance):
     pass
