329ee3b894016d286144b4bb16e5377b92c0b729
Russell Ballestrini <russell.ballestrini@gmail.com>
2017-06-21 09:50:26 -0700

    Porting internal Remind version of this blueprint
    	modified:   stacker_blueprints/route53.py
    	new file:   tests/fixtures/blueprints/route53_dnsrecords.json
    	new file:   tests/fixtures/blueprints/route53_dnsrecords_zone_name.json
    	modified:   tests/test_route53.py

--- stacker_blueprints/route53.py
+++ stacker_blueprints/route53.py
@@ -1,51 +1,107 @@
-from stacker.blueprints.base import Blueprint
-from stacker.blueprints.variables.types import TroposphereType
+from hashlib import md5
 
-from troposphere import route53
+from stacker.blueprints.base import Blueprint
+
+from troposphere import (
+    Ref,
+    Output,
+    GetAtt,
+    Join,
+    route53,
+)
+
+CLOUDFRONT_HOSTED_ZONE_ID = "Z2FDTNDATAQYW2"
+
+
+def get_record_set_md5(rs_name, rs_type):
+    """Accept record_set Name and Type. Return MD5 sum of these values."""
+    return md5(rs_name + rs_type).hexdigest()
+
+
+def add_hosted_zone_id_if_missing(record_set, hosted_zone_id):
+    """Add HostedZoneId to Trophosphere record_set object if missing."""
+    if not getattr(record_set, "HostedZoneId", None):
+        record_set.HostedZoneId = hosted_zone_id
+    return record_set
+
+
+def add_hosted_zone_id_for_cloudfront_alias_if_missing(record_set):
+    """Accept a record_set Troposphere object. Returns record_set object."""
+    # magic to automatically add the HostedZoneId for cloudfront.net aliases.
+    # http://docs.aws.amazon.com/AWSCloudFormation/latest
+    #       /UserGuide/aws-properties-route53-aliastarget.html
+    if getattr(record_set, "AliasTarget", None):
+        if not getattr(record_set.AliasTarget, "HostedZoneId", None):
+            if ".cloudfront.net" in record_set.AliasTarget.DNSName:
+                record_set.AliasTarget.HostedZoneId = CLOUDFRONT_HOSTED_ZONE_ID
+    return record_set
 
 
 class DNSRecords(Blueprint):
 
     VARIABLES = {
+        "HostedZoneId": {
+            "type": str,
+            "description": "The id of an existing HostedZone.",
+            "default": "",
+        },
+        "HostedZoneName": {
+            "type": str,
+            "description": "The name of a HostedZone to create and manage.",
+            "default": "",
+        },
         "RecordSets": {
-            "type": TroposphereType(route53.RecordSetType, many=True),
-            "description": "A dictionary of AWS::Route53::RecordSet types.""",
-        },
-        "DefaultHostedZoneId": {
-            "type": str,
-            "description": "If provided, this hosted zone id will be used for "
-                           "any record that doesn't provide one. Note: "
-                           "cannot be set if 'DefaultHostedZoneName' is set.",
-            "default": "",
-        },
-        "DefaultHostedZoneName": {
-            "type": str,
-            "description": "If provided, this hosted zone id will be used for "
-                           "any record that doesn't provide one. Note: "
-                           "cannot be set if 'DefaultHostedZoneId' is set.",
-            "default": "",
+            "type": list,
+            "description": "A list of dictionaries representing the attributes"
+                           "of a troposphere.route53.RecordSetType object.",
+            "default": []
         },
     }
 
+    def create_record_set(self, rs_dict):
+        """Accept a record_set dict. Return a Troposphere record_set object."""
+        record_set_md5 = get_record_set_md5(rs_dict["Name"], rs_dict["Type"])
+        rs = route53.RecordSetType.from_dict(record_set_md5, rs_dict)
+        rs = add_hosted_zone_id_if_missing(rs, self.hosted_zone_id)
+        rs = add_hosted_zone_id_for_cloudfront_alias_if_missing(rs)
+        self.template.add_resource(rs)
+        return rs
+
+    def create_record_sets(self, record_set_dicts):
+        """Accept list of record_set dicts.
+        Return list of record_set objects."""
+        record_set_objects = []
+        for record_set_dict in record_set_dicts:
+            record_set_objects.append(self.create_record_set(record_set_dict))
+        return record_set_objects
+
     def create_template(self):
-        t = self.template
         variables = self.get_variables()
-        default_zone_name = variables["DefaultHostedZoneName"]
-        default_zone_id = variables["DefaultHostedZoneId"]
+        hosted_zone_name = variables["HostedZoneName"]
+        hosted_zone_id = variables["HostedZoneId"]
 
-        if all([default_zone_name, default_zone_id]):
-            raise ValueError("Cannot specify both 'DefaultHostedZoneName' and "
-                             "'DefaultHostedZoneId' variables.")
+        if all([hosted_zone_name, hosted_zone_id]):
+            raise ValueError("Cannot specify both 'HostedZoneName' and "
+                             "'HostedZoneId' variables.")
 
-        for record_set in variables["RecordSets"]:
-            if default_zone_name and not getattr(record_set,
-                                                 "HostedZoneName",
-                                                 None):
-                record_set.HostedZoneName = default_zone_name
+        if not any([hosted_zone_name, hosted_zone_id]):
+            raise ValueError("Please specify either a 'HostedZoneName' or "
+                             "'HostedZoneId' variable.")
 
-            if default_zone_id and not getattr(record_set,
-                                               "HostedZoneId",
-                                               None):
-                record_set.HostedZoneId = default_zone_id
+        if hosted_zone_id:
+            self.hosted_zone_id = hosted_zone_id
 
-            t.add_resource(record_set)
+        else:
+            self.template.add_resource(
+                route53.HostedZone("HostedZone", Name=hosted_zone_name)
+            )
+            self.hosted_zone_id = Ref("HostedZone")
+            self.nameservers = Join(',', GetAtt("HostedZone", "NameServers"))
+            self.template.add_output(
+                    Output("NameServers", Value=self.nameservers))
+
+        self.template.add_output(
+                Output("HostedZoneId", Value=self.hosted_zone_id))
+
+        # return a list of troposphere record set objects.
+        return self.create_record_sets(variables["RecordSets"])
--- tests/test_route53.py
+++ tests/test_route53.py
@@ -1,87 +1,127 @@
-import unittest
-
-from stacker.blueprints.testutil import BlueprintTestCase
 from stacker.context import Context
 from stacker.variables import Variable
 
 from stacker_blueprints.route53 import DNSRecords
 
+from stacker.blueprints.testutil import BlueprintTestCase
 
-class TestRepositories(BlueprintTestCase):
+class TestRoute53(BlueprintTestCase):
     def setUp(self):
         self.ctx = Context({'namespace': 'test'})
 
-    def test_create_template(self):
-        blueprint = DNSRecords('test_route53_dnsrecords', self.ctx)
+    def test_create_template_hosted_zone_id(self):
+        blueprint = DNSRecords('route53_dnsrecords', self.ctx)
         blueprint.resolve_variables(
             [
                 Variable(
                     "RecordSets",
-                    {
-                        "TestHost": {
+                    [
+                        {
                             "Name": "host.testdomain.com.",
                             "Type": "A",
                             "ResourceRecords": ["10.0.0.1"],
                         },
-                        "TestHost2": {
+                        {
                             "Name": "host2.testdomain.com.",
                             "Type": "A",
                             "ResourceRecords": ["10.0.0.2"],
                         },
-                    }
-                )
+                    ]
+                ),
+                Variable("HostedZoneId", "fake_zone_id"),
             ]
         )
         blueprint.create_template()
         self.assertRenderedBlueprint(blueprint)
 
-    def test_specify_multiple_defaults_fail(self):
-        blueprint = DNSRecords('test_route53_dnsrecords_multiple_defaults',
+    def test_create_template_hosted_zone_name(self):
+        blueprint = DNSRecords('route53_dnsrecords_zone_name', self.ctx)
+        blueprint.resolve_variables(
+            [
+                Variable(
+                    "RecordSets",
+                    [
+                        {
+                            "Name": "host.testdomain.com.",
+                            "Type": "A",
+                            "ResourceRecords": ["10.0.0.1"],
+                        },
+                        {
+                            "Name": "host2.testdomain.com.",
+                            "Type": "A",
+                            "ResourceRecords": ["10.0.0.2"],
+                        },
+                    ]
+                ),
+                Variable("HostedZoneName", "testdomain.com"),
+            ]
+        )
+        blueprint.create_template()
+        self.assertRenderedBlueprint(blueprint)
+
+    def test_cloudfront_alias_adds_hosted_zone_id(self):
+        blueprint = DNSRecords('route53_dnsrecords_alias', self.ctx)
+        blueprint.resolve_variables(
+            [
+                Variable(
+                    "RecordSets",
+                    [
+                        {
+                            "Name": "host.testdomain.com.",
+                            "Type": "A",
+                            "AliasTarget": {
+                                "DNSName": "d123456789f.cloudfront.net.",
+                            },
+                        },
+                    ]
+                ),
+                Variable("HostedZoneId", "fake_zone_id"),
+            ]
+        )
+        record_sets = blueprint.create_template()
+        self.assertEqual(record_sets[0].AliasTarget.HostedZoneId, "Z2FDTNDATAQYW2")
+
+    def test_error_when_specify_both_hosted_zone_id_and_name(self):
+        blueprint = DNSRecords('route53_both_hosted_zone_id_and_name_error',
                                self.ctx)
         blueprint.resolve_variables(
             [
                 Variable(
                     "RecordSets",
-                    {
-                        "TestHost": {
+                    [
+                        {
                             "Name": "host.testdomain.com.",
                             "Type": "A",
                             "ResourceRecords": ["10.0.0.1"],
                         },
-                    }
+                    ]
                 ),
-                Variable("DefaultHostedZoneId", "fake_zone_id"),
-                Variable("DefaultHostedZoneName", "fake_zone_name"),
+                Variable("HostedZoneId", "fake_zone_id"),
+                Variable("HostedZoneName", "fake_zone_name"),
             ]
         )
         with self.assertRaises(ValueError):
             blueprint.create_template()
 
-    def test_valid_default(self):
-        blueprint = DNSRecords('test_route53_dnsrecords_defaults', self.ctx)
+    def test_error_when_specify_no_hosted_zone_id_or_name(self):
+        blueprint = DNSRecords('route53_missing_hosted_zone_id_or_name_error',
+                               self.ctx)
         blueprint.resolve_variables(
             [
                 Variable(
                     "RecordSets",
-                    {
-                        "TestHost": {
+                    [
+                        {
                             "Name": "host.testdomain.com.",
                             "Type": "A",
                             "ResourceRecords": ["10.0.0.1"],
                         },
-                        "TestHost2": {
-                            "Name": "host2.testdomain.com.",
-                            "Type": "A",
-                            "ResourceRecords": ["10.0.0.2"],
-                            "HostedZoneId": "set_zone_id",
-                        },
-                    }
+                    ]
                 ),
-                Variable("DefaultHostedZoneId", "fake_zone_id"),
             ]
         )
-        blueprint.create_template()
-        self.assertRenderedBlueprint(blueprint)
+        with self.assertRaises(ValueError):
+            blueprint.create_template()
 
 
 if __name__ == '__main__':
