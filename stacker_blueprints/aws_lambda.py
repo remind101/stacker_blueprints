eea507bf42c220da2bc7a6073cacb4681e84e073
Michael Barrett <loki77@gmail.com>
2017-10-30 14:04:26 -0700

    merge internal changes @ Remind into public blueprint
    This pulls in the event source mapping that we added to our internal
    blueprint at Remind, as well as cleans up the tests to make them a
    little easier to write/more consistent.

--- stacker_blueprints/aws_lambda.py
+++ stacker_blueprints/aws_lambda.py
@@ -1,3 +1,5 @@
+import logging
+
 from stacker.blueprints.base import Blueprint
 
 from stacker.blueprints.variables.types import TroposphereType
@@ -15,13 +17,85 @@ from troposphere import awslambda
 
 from troposphere import events
 
-from awacs.aws import Policy
+import awacs.logs
+import awacs.kinesis
+import awacs.dynamodb
+
+from awacs.aws import Statement, Allow, Policy
 from awacs.helpers.trust import get_lambda_assumerole_policy
 
 from .policies import (
     lambda_basic_execution_statements,
     lambda_vpc_execution_statements,
 )
+
+
+logger = logging.getLogger(name=__name__)
+
+
+def get_stream_action_type(stream_arn):
+    """Returns the awacs Action for a stream type given an arn
+
+    Args:
+        stream_arn (str): The Arn of the stream.
+
+    Returns:
+        :class:`awacs.aws.Action`: The appropriate stream type awacs Action
+            class
+
+    Raises:
+        ValueError: If the stream type doesn't match kinesis or dynamodb.
+    """
+
+    stream_type_map = {
+        "kinesis": awacs.kinesis.Action,
+        "dynamodb": awacs.dynamodb.Action,
+    }
+
+    stream_type = stream_arn.split(":")[2]
+    try:
+        return stream_type_map[stream_type]
+    except KeyError:
+        raise ValueError(
+            "Invalid stream type '%s' in arn '%s'" % (stream_type, stream_arn)
+        )
+
+
+def stream_reader_statements(stream_arn):
+    """Returns statements to allow Lambda to read from a stream.
+
+    Handles both DynamoDB & Kinesis streams. Automatically figures out the
+    type of stream, and provides the correct actions from the supplied Arn.
+
+    Arg:
+        stream_arn (str): A kinesis or dynamodb stream arn.
+
+    Returns:
+        list: A list of statements.
+    """
+    action_type = get_stream_action_type(stream_arn)
+    arn_parts = stream_arn.split("/")
+    # Cut off the last bit and replace it with a wildcard
+    wildcard_arn_parts = arn_parts[:-1]
+    wildcard_arn_parts.append("*")
+    wildcard_arn = "/".join(wildcard_arn_parts)
+
+    return [
+        Statement(
+            Effect=Allow,
+            Resource=[stream_arn],
+            Action=[
+                action_type("DescribeStream"),
+                action_type("GetRecords"),
+                action_type("GetShardIterator"),
+            ]
+        ),
+        Statement(
+            Effect=Allow,
+            Resource=[wildcard_arn],
+            Action=[action_type("ListStreams")]
+        )
+    ]
 
 
 class Function(Blueprint):
@@ -110,6 +184,11 @@ class Function(Blueprint):
                            "function Arn prepended.",
             "default": "$LATEST",
         },
+        "EventSourceMapping": {
+            "type": dict,
+            "description": "An optional event source mapping config.",
+            "default": {},
+        },
     }
 
     def code(self):
@@ -138,16 +217,46 @@ class Function(Blueprint):
             config = awslambda.VPCConfig(**vpc_config)
         return config
 
-    def generate_policy_statements(self):
-        """Generates the policy statements for the role used by the function.
+    def add_policy_statements(self, statements):
+        """Adds statements to the policy.
 
-        You should override this, and extend the list that it returns with
-        any additional iam policy statements you wish to give the role.
+        Args:
+            statements (:class:`awacs.aws.Statement` or list): Either a single
+                Statment, or a list of statements.
+        """
+        if isinstance(statements, Statement):
+            statements = [statements]
+        self._policy_statements.extend(statements)
+
+    def extended_policy_statements(self):
+        """Override this and add statements to add them to the lambda policy
 
         Returns:
             list: A list of :class:`awacs.aws.Statement` objects.
         """
-        return lambda_basic_execution_statements(Ref("Function"))
+        return []
+
+    def generate_policy_statements(self):
+        """Generates the policy statements for the role used by the function.
+
+        To add additional statements you can either override the
+        `extended_policy_statements` method to return a list of Statements
+        to be added to the policy, or override this method itself if you
+        need more control.
+
+        Returns:
+            list: A list of :class:`awacs.aws.Statement` objects.
+        """
+        statements = self._policy_statements
+        statements.extend(
+            lambda_basic_execution_statements(
+                self.function.Ref()
+            )
+        )
+        extended_statements = self.extended_policy_statements()
+        if extended_statements:
+            statements.extend(extended_statements)
+        return statements
 
     def create_policy(self):
         t = self.template
@@ -260,13 +369,51 @@ class Function(Blueprint):
 
             t.add_output(Output("AliasArn", Value=self.alias.Ref()))
 
+    def create_event_source_mapping(self):
+        t = self.template
+        variables = self.get_variables()
+        mapping = variables["EventSourceMapping"]
+        if mapping:
+            if "FunctionName" in mapping:
+                logger.warn(
+                    "FunctionName defined in EventSourceMapping in "
+                    "%s. Overriding.", self.context.get_fqn(self.name)
+                )
+            mapping["FunctionName"] = self.function.GetAtt("Arn")
+            resource = t.add_resource(
+                awslambda.EventSourceMapping.from_dict(
+                    "EventSourceMapping", mapping
+                )
+            )
+
+            if not variables["Role"]:
+                self.add_policy_statements(
+                    stream_reader_statements(
+                        mapping["EventSourceArn"]
+                    )
+                )
+
+            t.add_output(
+                Output("EventSourceMappingId", Value=resource.Ref())
+            )
+
     def create_template(self):
         variables = self.get_variables()
-        self.role_arn = variables["Role"]
-        if not variables["Role"]:
+        self._policy_statements = []
+        role_arn = variables["Role"]
+        # Set here - used in `create_role` to determine if an external role
+        # was passed in. If an external role is passed in, no new role is
+        # created, and now policies are generated/added to the external
+        # role.
+        self.role_arn = role_arn
+        if not role_arn:
             self.create_role()
-            self.create_policy()
         self.create_function()
+        self.create_event_source_mapping()
+        # We don't use self.role_arn here because it is set internally if a
+        # role is created
+        if not role_arn:
+            self.create_policy()
 
 
 class FunctionScheduler(Blueprint):
