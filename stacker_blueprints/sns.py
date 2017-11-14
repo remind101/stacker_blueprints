remind101/stacker_blueprints

commit f4f2edb12da7fd82342688c55ca5a62c656b8c24

Create sqs policies only for sqs-type topic subscriptions
sns.py tries to create an sqs policy for each subscription,
regardless of Protocol; which fails for subscriptions with protocol
other than "sqs".

Fix this by creating the policy only for sqs-type topic subscriptions.

Includes a test for sns.Topics.

stacker_blueprints/sns.py
@@ -133,5 +133,6 @@ class Topics(Blueprint):
         )
         t.add_output(Output(topic_name + "Arn", Value=topic_arn))
 
-        if topic_subs:
-            self.create_sqs_policy(topic_name, topic_arn, topic_subs)
+        sqs_subs = [sub for sub in topic_subs if sub["Protocol"] == "sqs"]
+        if sqs_subs:
+            self.create_sqs_policy(topic_name, topic_arn, sqs_subs)

tests/test_sns.py
@@ -1,0 +1,32 @@
+import unittest
+from stacker.context import Context
+from stacker.variables import Variable
+from stacker_blueprints.sns import Topics
+from stacker.blueprints.testutil import BlueprintTestCase
+
+class TestBlueprint(BlueprintTestCase):
+    def setUp(self):
+        self.variables = [
+            Variable('Topics', {
+                'Example': {
+                    'DisplayName': 'ExampleTopic',
+                    'Subscription': [
+                        {
+                            'Endpoint': 'arn:aws:sqs:us-east-1:123456788901:example-queue',
+                            'Protocol': 'sqs',
+                        },
+                        {
+                            'Endpoint': 'postmaster@example.com',
+                            'Protocol': 'email',
+                        },
+                    ]
+                },
+            }),
+        ]
+
+    def test_sns(self):
+        ctx = Context({'namespace': 'test', 'environment': 'test'})
+        blueprint = Topics('topics', ctx)
+        blueprint.resolve_variables(self.variables)
+        blueprint.create_template()
+        self.assertRenderedBlueprint(blueprint)

commit 4ec020a7da8020148624dc77a3c7cb11bd2b3a24

Fix issue with topics with no subscriptions
Fix an issue introduced in f4f2edb. sns.py will try to iterate
the subscriptions as a list, but when it's not specified,
topic_subs defaults to None. Default instead to [] so it can always
be iterated as a list.

Fixes #152.

stacker_blueprints/sns.py
@@ -113,7 +113,7 @@ class Topics(Blueprint):
         """
         Creates the SNS topic, along with any subscriptions requested.
         """
-        topic_subs = None
+        topic_subs = []
         t = self.template
 
         if "Subscription" in topic_config:

tests/test_sns.py
@@ -8,6 +8,9 @@ class TestBlueprint(BlueprintTestCase):
     def setUp(self):
         self.variables = [
             Variable('Topics', {
+                'WithoutSubscription': {
+                    'DisplayName': 'SampleTopicWithoutSub',
+                },
                 'Example': {
                     'DisplayName': 'ExampleTopic',
                     'Subscription': [