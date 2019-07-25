d9859dcede9bd0225db0fef6fcd0aec417b813e8
Aaron McMillin <github@aaron.mcmillinclan.org>
2019-07-25 00:01:27 -0400

    Update redis validation values

--- stacker_blueprints/elasticache/redis.py
+++ stacker_blueprints/elasticache/redis.py
@@ -7,7 +7,8 @@ class RedisReplicationGroup(base.BaseReplicationGroup):
 
     def get_engine_versions(self):
         return ["2.6.13", "2.8.19", "2.8.21", "2.8.22", "2.8.23", "2.8.24",
-                "2.8.6", "3.2.4"]
+                "2.8.6", "3.2.4", "3.2.6", "3.2.10", "4.0.10", "5.0.0",
+                "5.0.3", "5.0.4"]
 
     def get_parameter_group_family(self):
-        return ["redis2.6", "redis2.8", "redis3.2"]
+        return ["redis2.6", "redis2.8", "redis3.2", "redis4.0", "redis5.0"]
